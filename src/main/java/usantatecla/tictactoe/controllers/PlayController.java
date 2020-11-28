f76ba2f9a1be56f361c05351e5b330deb5986a76
Miguel G. Sanguino <sanguino@me.com>
2020-11-28 18:56:56 +0100

    commit base

---
+++ src/main/java/usantatecla/tictactoe/controllers/PlayController.java
@@ -0,0 +1,38 @@
+package usantatecla.tictactoe.controllers;
+
+import usantatecla.tictactoe.models.Coordinate;
+import usantatecla.tictactoe.types.Error;
+import usantatecla.tictactoe.models.Game;
+import usantatecla.tictactoe.models.Token;
+
+public class PlayController extends Controller {
+
+	public PlayController(Game game) {
+		super(game);
+	}
+
+	public boolean isBoardComplete() {
+		return this.game.isBoardComplete();
+	}
+
+	public boolean isTicTacToe() {
+		return this.game.isTicTacToe();
+	}
+
+	public Token getToken() {
+		return this.game.getToken();
+	}
+
+	public boolean isUser() {
+		return this.game.isUser();
+	}
+
+	public Error put(Coordinate coordinate) {
+		return this.game.put(coordinate);
+	}
+
+	public Error move(Coordinate origin, Coordinate target) {
+		return this.game.move(origin, target);
+	}
+
+}
---
+++ src/main/java/usantatecla/tictactoe/views/console/PlayView.java
@@ -0,0 +1,73 @@
+package usantatecla.tictactoe.views.console;
+
+import usantatecla.tictactoe.controllers.PlayController;
+import usantatecla.tictactoe.models.Coordinate;
+import usantatecla.tictactoe.views.Message;
+import usantatecla.tictactoe.types.Error;
+
+class PlayView {
+
+    private PlayController playController;
+
+    PlayView(PlayController playController) {
+        this.playController = playController;
+    }
+
+    void interact() {
+        do {
+            //this.playController.next();
+            if (!this.playController.isBoardComplete()) {
+                this.put();
+            } else {
+                this.move();
+            }
+            new GameView(this.playController).write();
+        } while (!this.playController.isTicTacToe());
+        new TokenView(this.playController.getToken()).write();
+        Message.PLAYER_WIN.writeln();
+    }
+
+    private void put() {
+        boolean isUser = this.playController.isUser();
+        Coordinate coordinate;
+        Error error;
+        do {
+            if (isUser) {
+                coordinate = new CoordinateView().read(Message.COORDINATE_TO_PUT.toString());
+            } else {
+                coordinate = createRandomCoordinate();
+            }
+            error = this.playController.put(coordinate);
+            if (isUser) {
+                new ErrorView(error).writeln();
+            }
+        } while (!error.isNull());
+    }
+
+    private void move() {
+        boolean isUser = this.playController.isUser();
+        Coordinate origin;
+        Coordinate target;
+        Error error;
+        do {
+            if (isUser) {
+                origin = new CoordinateView().read(Message.COORDINATE_TO_REMOVE.toString());
+                target = new CoordinateView().read(Message.COORDINATE_TO_MOVE.toString());
+            } else {
+                origin = createRandomCoordinate();
+                target = createRandomCoordinate();
+            }
+            error = this.playController.move(origin, target);
+            if (isUser) {
+                new ErrorView(error).writeln();
+            }
+        } while (!error.isNull());
+    }
+
+    Coordinate createRandomCoordinate() {
+        Coordinate coordinate = new Coordinate();
+        coordinate.random();
+        return coordinate;
+    }
+
+}
