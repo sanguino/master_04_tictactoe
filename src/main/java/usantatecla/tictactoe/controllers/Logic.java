3eca2af23ce207a31180e6d7fabf9a11060e205f
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 19:21:07 +0100

    red, create map with controller-state relation

--- src/main/java/usantatecla/tictactoe/controllers/Logic.java
+++ src/main/java/usantatecla/tictactoe/controllers/Logic.java
@@ -1,13 +1,23 @@
 package usantatecla.tictactoe.controllers;
 
 import usantatecla.tictactoe.models.Game;
+import usantatecla.tictactoe.types.StateValue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Logic {
 
   private Game game;
+  private Map<StateValue, Controller> controllers;
 
   public Logic() {
     this.game = new Game();
+    this.controllers = new HashMap<StateValue, Controller>();
+    this.controllers.put(StateValue.INITIAL, new StartController(this.game));
+    this.controllers.put(StateValue.IN_GAME, new PlayController(this.game));
+    this.controllers.put(StateValue.RESUME, new ResumeController(this.game));
+    this.controllers.put(StateValue.EXIT, null);
   }
 
   public Controller getController() {
