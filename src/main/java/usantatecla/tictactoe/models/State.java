9ddd09c8035e731430448dab0bf3a92e80148b8f
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 18:30:17 +0100

    red, create state, getValueState and test

---
+++ src/main/java/usantatecla/tictactoe/models/State.java
@@ -0,0 +1,17 @@
+package usantatecla.tictactoe.models;
+
+import usantatecla.tictactoe.types.StateValue;
+
+public class State {
+
+    private StateValue stateValue;
+
+    public State() {
+
+    }
+
+    public StateValue getValueState() {
+        return this.stateValue;
+    }
+
+}
---
+++ src/test/java/usantatecla/tictactoe/models/StateTest.java
@@ -0,0 +1,24 @@
+package usantatecla.tictactoe.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import usantatecla.tictactoe.types.StateValue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StateTest {
+    
+    private State state;
+
+    @BeforeEach
+    void before() {
+        this.state = new State();
+    }
+
+    @Test
+    public void testGivenNewStateWhenGetValueStateThenIsInitial() {
+        assertEquals(StateValue.INITIAL, this.state.getValueState());
+    }
+
+
+}
