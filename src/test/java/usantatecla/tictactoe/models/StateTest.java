b5801d53d5c31571dbd8df9817e6b4cff933ef50
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 18:33:22 +0100

    green, add more test to next()

--- src/test/java/usantatecla/tictactoe/models/StateTest.java
+++ src/test/java/usantatecla/tictactoe/models/StateTest.java
@@ -26,4 +26,19 @@ public class StateTest {
         assertEquals(StateValue.IN_GAME, this.state.getValueState());
     }
 
+    @Test
+    public void testGivenNewStateWhenDoTwoNextAndGetValueStateThenIsResult() {
+        this.state.next();
+        this.state.next();
+        assertEquals(StateValue.RESUME, this.state.getValueState());
+    }
+
+    @Test
+    public void testGivenNewStateWhenDoThreeNextAndGetValueStateThenIsExit() {
+        this.state.next();
+        this.state.next();
+        this.state.next();
+        assertEquals(StateValue.EXIT, this.state.getValueState());
+    }
+
 }
