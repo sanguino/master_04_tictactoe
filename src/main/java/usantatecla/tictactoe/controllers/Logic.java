0c941fda4b860d46725bc237a4df37bebbc936c7
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 22:03:33 +0100

    green, use AcceptorController in logic

--- src/main/java/usantatecla/tictactoe/controllers/Logic.java
+++ src/main/java/usantatecla/tictactoe/controllers/Logic.java
@@ -9,18 +9,18 @@ import java.util.Map;
 public class Logic {
 
   private Game game;
-  private Map<StateValue, Controller> controllers;
+  private Map<StateValue, AcceptorController> controllers;
 
   public Logic() {
     this.game = new Game();
-    this.controllers = new HashMap<StateValue, Controller>();
+    this.controllers = new HashMap<StateValue, AcceptorController>();
     this.controllers.put(StateValue.INITIAL, new StartController(this.game));
     this.controllers.put(StateValue.IN_GAME, new PlayController(this.game));
     this.controllers.put(StateValue.RESUME, new ResumeController(this.game));
     this.controllers.put(StateValue.EXIT, null);
   }
 
-  public Controller getController() {
+  public AcceptorController getController() {
     return this.controllers.get(this.game.getValueState());
   }
 
