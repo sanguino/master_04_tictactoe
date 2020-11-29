d88cbbbadc5d2819c1c4b83dd3a1f5f378738e1e
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 20:27:49 +0100

    green, test refactor to add tests

--- src/test/java/usantatecla/tictactoe/models/LogicTest.java
+++ src/test/java/usantatecla/tictactoe/models/LogicTest.java
@@ -2,24 +2,44 @@ package usantatecla.tictactoe.models;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import usantatecla.tictactoe.controllers.Logic;
+import usantatecla.tictactoe.controllers.PlayController;
 import usantatecla.tictactoe.controllers.StartController;
+import usantatecla.tictactoe.types.StateValue;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
+@ExtendWith(MockitoExtension.class)
 public class LogicTest {
 
+    @Mock
+    private Game game;
+
+    @InjectMocks
     private Logic logic;
 
     @BeforeEach
     void before() {
-        this.logic = new Logic();
+        openMocks(this);
     }
 
     @Test
     public void testGivenNewLogicWhenGetControllerThenIsStartController() {
-        assertThat(logic.getController(), instanceOf(StartController.class));
+        when(this.game.getValueState()).thenReturn(StateValue.INITIAL);
+        assertThat(this.logic.getController(), instanceOf(StartController.class));
+    }
+
+    @Test
+    public void testGivenNewLogicWhenGameNextAndGetControllerThenIsPlayController() {
+        when(this.game.getValueState()).thenReturn(StateValue.IN_GAME);
+        assertThat(logic.getController(), instanceOf(PlayController.class));
     }
 
 }
