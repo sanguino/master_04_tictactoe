c587b184a410ec2519f10646c00a7ffd79d73f2b
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 22:13:41 +0100

    green, use ControllerVisitor in View

--- src/main/java/usantatecla/tictactoe/views/console/View.java
+++ src/main/java/usantatecla/tictactoe/views/console/View.java
@@ -1,10 +1,11 @@
 package usantatecla.tictactoe.views.console;
 
+import usantatecla.tictactoe.controllers.ControllerVisitor;
 import usantatecla.tictactoe.controllers.PlayController;
 import usantatecla.tictactoe.controllers.ResumeController;
 import usantatecla.tictactoe.controllers.StartController;
 
-public class View extends usantatecla.tictactoe.views.View {
+public class View extends usantatecla.tictactoe.views.View implements ControllerVisitor {
 
 	private StartView startView;
 	private PlayView playView;
@@ -29,5 +30,20 @@ public class View extends usantatecla.tictactoe.views.View {
 	@Override
 	public boolean interact(ResumeController resumeController){
 		return this.resumeView.interact(resumeController);
+	}
+
+	@Override
+	public void visit(StartController startController) {
+		this.startView.interact(startController);
+	}
+
+	@Override
+	public void visit(PlayController playController) {
+		this.playView.interact(playController);
+	}
+
+	@Override
+	public void visit(ResumeController resumeController) {
+		this.resumeView.interact(resumeController);
 	}
 }
