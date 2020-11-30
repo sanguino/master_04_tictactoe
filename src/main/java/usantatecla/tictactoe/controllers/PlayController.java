a0fdd36c0af13cb5d5ebd47f1496afdcb23e8c38
Miguel G. Sanguino <sanguino@me.com>
2020-11-30 23:53:42 +0100

    copy add controllers to PlayController and wrap the use of the functions

--- src/main/java/usantatecla/tictactoe/controllers/PlayController.java
+++ src/main/java/usantatecla/tictactoe/controllers/PlayController.java
@@ -7,40 +7,39 @@ import usantatecla.tictactoe.types.Error;
 
 public class PlayController extends Controller implements AcceptorController{
 
+	private ActionController actionController;
+	private UndoController undoController;
+	private RedoController redoController;
+
 	public PlayController(Session session) {
 		super(session);
+		this.actionController = new ActionController(session);
+		this.undoController = new UndoController(session);
+		this.redoController = new RedoController(session);
 	}
 
 	public boolean isBoardComplete() {
-		return this.session.isBoardComplete();
+		return this.actionController.isBoardComplete();
 	}
 
 	public boolean isTicTacToe() {
-		return this.session.isTicTacToe();
+		return this.actionController.isTicTacToe();
 	}
 
 	public Token getToken() {
-		return this.session.getToken();
+		return this.actionController.getToken();
 	}
 
 	public boolean isUser() {
-		return this.session.isUser();
+		return this.actionController.isUser();
 	}
 
 	public Error put(Coordinate coordinate) {
-		Error error = this.session.put(coordinate);
-		if (error.isNull() && this.session.isTicTacToe()) {
-			this.session.next();
-		}
-		return error;
+		return this.actionController.put(coordinate);
 	}
 
 	public Error move(Coordinate origin, Coordinate target) {
-		Error error = this.session.move(origin, target);
-		if (error.isNull() && this.session.isTicTacToe()) {
-			this.session.next();
-		}
-		return error;
+		return this.actionController.move(origin, target);
 	}
 
 	@Override
