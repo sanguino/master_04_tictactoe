76326bf4ec9cf657bd1a191ee93d7f21270efe52
Miguel G. Sanguino <sanguino@me.com>
2020-12-01 12:22:03 +0100

    green, add playCommand to Play menu

--- src/main/java/usantatecla/tictactoe/views/console/PlayMenu.java
+++ src/main/java/usantatecla/tictactoe/views/console/PlayMenu.java
@@ -6,6 +6,7 @@ import usantatecla.utils.Menu;
 public class PlayMenu extends Menu {
 
     PlayMenu(PlayController playController) {
+        this.addCommand(new PlayCommand(playController));
         this.addCommand(new UndoCommand(playController));
         this.addCommand(new RedoCommand(playController));
     }
