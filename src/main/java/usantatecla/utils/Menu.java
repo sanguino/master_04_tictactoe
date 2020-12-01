21ac6fac7c89d10cc6ad48419f1ec399b34a35ff
Miguel G. Sanguino <sanguino@me.com>
2020-12-01 01:30:22 +0100

    green, refactor Menu addCommand

--- src/main/java/usantatecla/utils/Menu.java
+++ src/main/java/usantatecla/utils/Menu.java
@@ -1,9 +1,14 @@
 package usantatecla.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Menu {
 
-    public Menu() {
+    private List<Command> commandList;
 
+    public Menu() {
+        this.commandList = new ArrayList<>();
     }
 
     public void execute() {
@@ -12,11 +17,14 @@ public abstract class Menu {
         do {
             error = false;
             console.writeln();
+            for (int i = 0; i < commandList.size(); i++) {
+                console.writeln((i + 1) + ") " + commandList.get(i).getTitle());
+            }
 
         } while (error);
     }
 
     protected void addCommand(Command command) {
-
+        this.commandList.add(command);
     }
 }
